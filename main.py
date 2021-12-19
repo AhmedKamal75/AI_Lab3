8c221e51c7b9288311281a8872bf4039341e3650
Adel <56457765+aadelafattah@users.noreply.github.com>
2021-12-19 13:44:45 +0200

    data balancing

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
+import random
 import pandas as pd
-import pandas as np
 
 dataset = pd.read_csv("data/magic04.data").to_numpy()
 
-h = 0
-g = 0
-others = 0
+h = []
+g = []
+# others = 0
 
-for c in dataset[:, -1]:
-    if c == 'h':
-        h += 1
-    elif c == 'g':
-        g += 1
+
+# for c in dataset[:, -1]:
+#     if c == 'h':
+#         h += 1
+#     elif c == 'g':
+#         g += 1
+#     else:
+#         others += 1
+#
+# print(f"# of h = {h}")
+# print(f"# of g = {g}")
+# print(f"# of others = {others}")
+
+
+for c in range(len(dataset)):
+    if dataset[c][-1] == 'h':
+        h.append(dataset[c])
+    elif dataset[c][-1] == 'g':
+        g.append(dataset[c])
+
+
+def data_balancing(a, b):
+    if len(a) > len(b):
+        return b, random.choices(a, k=len(b))
     else:
-        others += 1
+        return a, random.choices(b, k=len(a))
 
-print(f"# of h = {h}")
-print(f"# of g = {g}")
-print(f"# of others = {others}")
+
+hard_on_list, gamma_list = data_balancing(h, g);
+
+def data_splitting():
