64ba33f312b35a4136afaa51e5d52e34f34c7c49
AhmedKamal <rtya89@gmail.com>
2021-12-20 18:39:11 +0200

    naive bayes complete, with torch

--- Naive_Bayes.py
+++ Naive_Bayes.py
@@ -1,5 +1,79 @@
+import math
+from scipy.stats import norm
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import f1_score, recall_score, accuracy_score, precision_score, confusion_matrix
+import torch
+
+
+class NaiveBayesGaussian:
+    def __init__(self):
+        self.ones_num = 0
+        self.zero_num = 0
+        self.ones_mean = None
+        self.zero_mean = None
+        self.ones_std = None
+        self.zero_std = None
+
+    def fit(self, X, y):
+        X = torch.tensor(X)
+        y = torch.tensor(y)
+        self.ones_num = torch.count_nonzero(y).item()
+        self.zero_num = y.size()[0] - self.ones_num
+        X_0 = torch.zeros((self.zero_num, X.shape[1]))
+        X_1 = torch.zeros((self.ones_num, X.shape[1]))
+
+        i = 0
+        j = 0
+        for col, cls in enumerate(y):
+            if cls == 1:
+                X_1[i, :] = X[col, :]
+                i += 1
+            else:
+                X_0[j, :] = X[col, :]
+                j += 1
+
+        self.zero_mean = torch.mean(X_0, dim=0)
+        self.ones_mean = torch.mean(X_1, dim=0)
+        self.zero_std = torch.std(X_0, dim=0)
+        self.ones_std = torch.std(X_1, dim=0)
+
+    def predict(self, X):
+        predictions = torch.ones((X.shape[0], 1))
+        for i, instance in enumerate(X):
+            if self.predict_instance(instance) == 0:
+                predictions[i, 0] = 0
+        return predictions.numpy()
+
+    def predict_instance(self, x):
+        score_0 = math.log(self.zero_num / (self.zero_num + self.ones_num))
+
+        for (point, mean, std) in zip(x, self.zero_mean, self.zero_std):
+            score_0 += norm.logpdf(point, loc=mean, scale=std)
+
+        score_1 = math.log(self.ones_num / (self.zero_num + self.ones_num))
+        for (point, mean, std) in zip(x, self.ones_mean, self.ones_std):
+            score_1 += norm.logpdf(point, loc=mean, scale=std)
+
+        if score_1 > score_0:
+            return 1.0
+        else:
+            return 0.0
+
+
+def naive_bayes_gaussian(X, y, X_test, y_test):
+    model = NaiveBayesGaussian()
+    model.fit(X, y)
+
+    predictions = model.predict(X_test)
+    # good = 0
+    # bad = 0
+    # for (true, pre) in zip(y_test, predictions):
+    #     if true[0] == pre[0]:
+    #         good += 1
+    #     else:
+    #         bad += 1
+    # print(f"good: {good}, bad: {bad}")
+    return get_result(y_test, predictions)
 
 
 def naive_bayes_sklearn(X, y, X_test, y_test):
@@ -7,14 +81,19 @@ def naive_bayes_sklearn(X, y, X_test, y_test):
     model.fit(X, y)
     predictions = model.predict(X_test)
 
-    tn, fp, fn, tp = confusion_matrix(y_test, predictions)
+    return get_result(y_test, predictions)
+
+
+def get_result(true, predictions):
+    (tn, fp), (fn, tp) = confusion_matrix(true, predictions)
     result = {
-        "f": f1_score(y_test, predictions, pos_label="h"),
-        "recall": recall_score(y_test, predictions, pos_label="h"),
-        "accuracy": accuracy_score(y_test, predictions),
-        "precision": precision_score(y_test, predictions, pos_label="h"),
+        "f": f1_score(true, predictions, pos_label=1.0),
+        "recall": recall_score(true, predictions, pos_label=1.0),
+        "accuracy": accuracy_score(true, predictions),
+        "precision": precision_score(true, predictions, pos_label=1.0),
         "tn": tn,
         "fp": fp,
         "fn": fn,
         "tp": tp
     }
+    return result
--- akwork.py
+++ akwork.py
@@ -2,28 +2,47 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from imblearn.under_sampling import RandomUnderSampler
-from Naive_Bayes import naive_bayes_sklearn
+from Naive_Bayes import naive_bayes_gaussian
 
 
-def ready_data(X, y):
+def ready_data(dataset):
+    inputs = np.array(dataset.values[:, 0:-1], dtype=np.float32)
+    temp_target = dataset.values[:, -1]
+    for i, j in enumerate(temp_target):
+        if j == 'h':
+            temp_target[i] = 0.0
+        else:
+            temp_target[i] = 1.0
+    targets = np.array(temp_target, dtype=np.float16)
+    targets = targets.reshape((targets.shape[0], 1))
     # under sampling
-    rus = RandomUnderSampler(random_state=0, replacement=True)
-    X_res, y_res = rus.fit_resample(X, y)
+    # rus = RandomUnderSampler(random_state=0, replacement=True)
+    # X_res, y_res = rus.fit_resample(inputs, targets)
 
     # splitting
     # random_state=0 means that every time we have the same set of selected
-    return train_test_split(X_res, y_res, test_size=0.3, random_state=0)
+    # return train_test_split(X_res, y_res, test_size=0.3, random_state=0)
+    return inputs, targets
 
 
 def main(dataset, method="NB"):
+    global result
+    inputs, targets = ready_data(dataset)
     if method == "NB":
         # no need to resample any thing
-        X_train, X_test, y_train, y_test = train_test_split(dataset.values[:, 0:-1], dataset.values[:, -1],
-                                                            test_size=0.3,
-                                                            random_state=0)
-        result = naive_bayes_sklearn(X_train, y_train, X_test, y_test)
-    else:
-        X_train, X_test, y_train, y_test = ready_data(dataset.values[:, 0:-1], dataset.values[:, -1])
+        X_train, X_test, y_train, y_test = train_test_split(inputs, targets, test_size=0.3, random_state=0)
+        result = naive_bayes_gaussian(X_train, y_train, X_test, y_test)
+    elif method == "DT":
+        pass
+    elif method == "RF":
+        pass
+    elif method == "AB":
+        pass
+    elif method == "KNN":
+        pass
+
+    for title in result:
+        print(f"{title} --> {result.get(title)}")
 
 
 if __name__ == "__main__":
