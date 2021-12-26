7a9887d66892a381e4a0bff8d7956231b069579e
Adel <56457765+aadelafattah@users.noreply.github.com>
2021-12-26 19:05:54 +0200

    Random forests

--- random_forest.py
+++ random_forest.py
@@ -2,7 +2,8 @@ import random
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
 dataset = pd.read_csv("data/magic04.data",
@@ -30,14 +31,20 @@ hard_on_list, gamma_list = data_balancing(h, g);
 all_data = hard_on_list + gamma_list;
 balanced_dataset = pd.DataFrame(all_data)
 
-X = balanced_dataset.drop('Class', axis=1)
-y = balanced_dataset['Class']
+X = balanced_dataset.iloc[:, 0:10].values
+y = balanced_dataset.iloc[:, 10].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
-classifier = DecisionTreeClassifier()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+classifier = RandomForestClassifier(n_estimators=100, random_state=0)
 classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 
-print(confusion_matrix(y_test, y_pred))
-print(classification_report(y_test, y_pred))
+print(confusion_matrix(y_test,y_pred))
+print(classification_report(y_test,y_pred))
 print(accuracy_score(y_test, y_pred))
+
