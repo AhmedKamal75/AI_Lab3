6cde56283b52bf8dd261ab0c2f37c7d3ebd3d2c6
Adel <56457765+aadelafattah@users.noreply.github.com>
2021-12-27 18:19:54 +0200

    Update final.py

--- final.py
+++ final.py
@@ -35,11 +35,11 @@ def data_balancing(a, b):
 hard_on_list, gamma_list = data_balancing(h, g);
 all_data = hard_on_list + gamma_list;
 balanced_dataset = pd.DataFrame(all_data)
-X = balanced_dataset.drop('Class', axis=1)
-y = balanced_dataset['Class']
 
 
-def spliting_scaling():
+def spliting_scaling(dataset):
+    X = dataset.drop('Class', axis=1)
+    y = dataset['Class']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
     scaler = StandardScaler()
     scaler.fit(X_train)
@@ -48,10 +48,11 @@ def spliting_scaling():
     return X_train, X_test, y_train, y_test
 
 
+# dataset: balanced dataset or just the raw dataset
 # criterion :{“gini”, “entropy”},   default=”gini”
 # splitter  :{“best”, “random”},    default=”best”
-def decision_tree(criterion, splitter):
-    X_train, X_test, y_train, y_test = spliting_scaling()
+def decision_tree(dataset, criterion, splitter):
+    X_train, X_test, y_train, y_test = spliting_scaling(dataset)
     classifier = DecisionTreeClassifier(criterion=criterion, splitter=splitter)
     classifier.fit(X_train, y_train)
     y_pred = classifier.predict(X_test)
@@ -61,8 +62,8 @@ def decision_tree(criterion, splitter):
 
 
 # n_estimators  :int,   default=50
-def ada_boost(n_estimators):
-    X_train, X_test, y_train, y_test = spliting_scaling()
+def ada_boost(dataset, n_estimators):
+    X_train, X_test, y_train, y_test = spliting_scaling(dataset)
     classifier = AdaBoostClassifier(n_estimators=n_estimators)
     classifier.fit(X_train, y_train)
     y_pred = classifier.predict(X_test)
@@ -72,8 +73,8 @@ def ada_boost(n_estimators):
 
 
 # n_neighbors :int,     default=5
-def k_nn(n_neighbors):
-    X_train, X_test, y_train, y_test = spliting_scaling()
+def k_nn(dataset, n_neighbors):
+    X_train, X_test, y_train, y_test = spliting_scaling(dataset)
     classifier = KNeighborsClassifier(n_neighbors=n_neighbors)
     classifier.fit(X_train, y_train)
     y_pred = classifier.predict(X_test)
@@ -84,8 +85,8 @@ def k_nn(n_neighbors):
 
 # criterion :{“gini”, “entropy”},   default=”gini”
 # n_estimators  :int,   default=100
-def random_forests(criterion, n_estimators):
-    X_train, X_test, y_train, y_test = spliting_scaling()
+def random_forests(dataset, criterion, n_estimators):
+    X_train, X_test, y_train, y_test = spliting_scaling(dataset)
     classifier = RandomForestClassifier(criterion=criterion, n_estimators=n_estimators)
     classifier.fit(X_train, y_train)
     y_pred = classifier.predict(X_test)
@@ -94,8 +95,8 @@ def random_forests(criterion, n_estimators):
     print(accuracy_score(y_test, y_pred))
 
 
-def naive_bayes():
-    X_train, X_test, y_train, y_test = spliting_scaling()
+def naive_bayes(dataset):
+    X_train, X_test, y_train, y_test = spliting_scaling(dataset)
     classifier = GaussianNB()
     classifier.fit(X_train, y_train)
     y_pred = classifier.predict(X_test)
@@ -103,19 +104,39 @@ def naive_bayes():
     print(classification_report(y_test, y_pred))
     print(accuracy_score(y_test, y_pred))
 
-
+print("UNBALANCED_DATA")
+print("------------------")
 print("1)decision_tree()")
-decision_tree("gini", "best")
+decision_tree(dataset, "gini", "best")
 print("------------------")
 print("2)ada_boost()")
-ada_boost(50)
+ada_boost(dataset, 50)
 print("------------------")
 print("3)k_nn()")
-k_nn(5)
+k_nn(dataset, 5)
 print("------------------")
 print("4)random_forests()")
-random_forests("gini", 100)
+random_forests(dataset, "gini", 100)
 print("------------------")
 print("5)naive_bayes()")
-naive_bayes()
+naive_bayes(dataset)
+print("------------------")
+
+
+print("BALANCED_DATA")
+print("------------------")
+print("1)decision_tree()")
+decision_tree(balanced_dataset, "gini", "best")
+print("------------------")
+print("2)ada_boost()")
+ada_boost(balanced_dataset, 50)
+print("------------------")
+print("3)k_nn()")
+k_nn(balanced_dataset, 5)
+print("------------------")
+print("4)random_forests()")
+random_forests(balanced_dataset, "gini", 100)
+print("------------------")
+print("5)naive_bayes()")
+naive_bayes(balanced_dataset)
 print("------------------")
